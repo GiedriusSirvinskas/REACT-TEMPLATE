6df1cbe6fbf8b435404854ed56cdfc78bcbac683
GiedriusSirvinskas <giedrius64@gmail.com>
2023-02-20 17:04:59 +0200

    REACT TEMPLATE

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Header";
+import Cards from "./Cards";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
